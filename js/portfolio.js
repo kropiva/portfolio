849f2cfab95f800ed239463f32d61dfe98d0a275
shershni@gmail.com <shershni@gmail.com>
2016-08-22 19:37:29 +0300

    my first commit

---
+++ js/portfolio.js
@@ -0,0 +1,13 @@
+/**
+ * Created by к on 26.05.2016.
+ */
+function showMyDiv() {
+   var sig = document.getElementById("sign");
+    alert(sig);
+}
+function show(state){
+
+    document.getElementById('sign').style.display = state;
+    document.getElementById('opacity-block').style.display = state;
+}
+;
